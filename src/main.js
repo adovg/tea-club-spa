78c5db7533a42deeaa9b17607c104c26d5f9014f
adovg <kroatoan@i.ua>
2024-05-07 23:13:57 +0300

    add blog feature

--- src/main.js
+++ src/main.js
@@ -1,21 +1,24 @@
-import '../src/styles.scss';
-
-
+import "../src/styles.scss";
 
 window.onload = () => {
-  console.log('hello world')
   const burgerBtn = document.querySelector(".burger__btn");
   const menuMobile = document.querySelector(".menu__mobile");
-  const accordion = document.getElementsByClassName("tab");
+  const blogBtn = document.querySelector(".blog__all");
+  let blogItem = document.querySelector(".blog__row-hidden");
+
+  window.addEventListener("load", () => {
+    alert("Страница загружена!");
+    showBlockItem();
+  });
 
   burgerBtn.onclick = function () {
-    burgerBtn.classList.toggle("active");
+    //burgerBtn.classList.toggle("active");
     hideMenu();
   };
 
   menuMobile.onclick = function () {
     hideMenu();
-    burgerBtn.classList.remove("active");
+    //burgerBtn.classList.remove("active");
   };
 
   function hideMenu() {
@@ -23,5 +26,20 @@ window.onload = () => {
     menuMobile.classList.toggle("hidden");
   }
 
- 
+  function showBlockItem() {
+    let screenWidth = window.innerWidth;
+
+    if (screenWidth < 360) {
+      console.log("first");
+      blogItem.classList.add("hidden");
+    }
+  }
+
+  showBlockItem();
+
+  blogBtn.onclick = function () {
+    blogItem.classList.toggle("hidden");
+  };
 };
+
+//blog__row-hidden
